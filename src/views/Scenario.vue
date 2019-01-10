bed043162ab01aae58380ae5526fad6353280b51
Benjamin Martin <ben@martindevnow.com>
2019-01-10 12:19:14 -0500

    Add interactions

--- src/views/Scenario.vue
+++ src/views/Scenario.vue
@@ -1,8 +1,25 @@
 <template>
   <div v-if="scenario">
+    
     <h2>{{ scenario.name }}</h2>
     <p>{{ scenario.introText }}</p>
-    <qrcode-stream @decode="onDecode"></qrcode-stream>
+
+    <div class="startQuestioning" v-if="!isQuestioning">
+      <button @click="talkToPerson = true">Talk to Someone</button>
+      <qrcode-stream v-if="talkToPerson" @decode="onQuestionIndividual"></qrcode-stream>
+    </div>
+
+    <div class="isQuestioning" v-if="isQuestioning">
+      <p>{{ this.questioningText }}</p>
+      <button @click="askQuestion = true" v-if="!response">Ask About...</button>
+      <qrcode-stream v-if="askQuestion" @decode="onAskQuestion"></qrcode-stream>
+    </div>
+
+    <div class="response" v-if="response">
+      <p>{{ response }}</p>
+      <button @click="askQuestion = true">Ask a follow up question...</button>
+    </div>
+
   </div>
 </template>
 
@@ -12,6 +29,11 @@ import { mapGetters } from 'vuex';
 export default {
   data() {
     return {
+      talkToPerson: false,
+      askQuestion: false,
+      isQuestioning: false,
+      questioningText: '',
+      response: '',
     };
   },
    computed: {
@@ -20,8 +42,13 @@ export default {
     ]),
   },
   methods: {
-    onDecode(txt) {
-      alert(txt);
+    onQuestionIndividual(txt) {
+      this.questioningText = `You are now questioning: ${txt}`;
+      this.isQuestioning = true;
+    },
+    onAskQuestion(txt) {
+      this.askQuestion = false;
+      this.response = `This is a hard coded response.. sorry, no info here. You asked about ${txt}`;
     }
   }
 }
