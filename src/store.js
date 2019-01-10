2553eb76fe2c05130e98cde2ae0307194fe0bd6c
Benjamin Martin <ben@martindevnow.com>
2019-01-09 21:18:00 -0500

    Initial Commit

--- src/store.js
+++ src/store.js
@@ -5,12 +5,23 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    current: {
+      scenarioId: 0,
+    },
+    scenarios: [
+      { id: 1, name: 'Fear the Living', introText: 'You walk into the lorem ipsum dolor sit amet' },
+    ]
+  },
+  getters: {
+    scenarios: (state) => state.scenarios,
+    scenarioById: (state, id) => state.scenarios.find(scenario => scenario.id === id),
   },
   mutations: {
-
+    selectScenario: (state, id) => state.current = { ...state.current, id: id },
   },
   actions: {
-
+    selectScenario: ({ commit }, id) => {
+      commit('selectScenario', id);
+    }
   }
 })
---
+++ src/views/Scenarios.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="scenario-select">
+    <h1>Select a Scenario</h1>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/scenarios">Scenarios</router-link>
+    </div>
+    <div id="scenario-list">
+      <ul>
+        <li v-for="scenario in scenarios" :key="scenario.id" 
+            @click="select(scenario.id)">{{scenario.name}}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapMutations } from 'vuex';
+
+export default {
+  computed: {
+    ...mapGetters([
+      'scenarios',
+    ]),
+  },
+  methods: {
+    ...mapMutations([
+      'selectScenario',
+    ]),
+    select: (id) => {
+      this.selectScenario(id);
+      this.$router.push({name: 'scenario'});
+    },
+  }
+}
+</script>
