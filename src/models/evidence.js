81bbe07dd2edfb6e4bc23b5fafce6e1e4d380176
Benjamin Martin <ben@martindevnow.com>
2019-01-12 13:28:34 -0500

    Adding in Reference Material

---
+++ src/models/evidence.js
@@ -0,0 +1,41 @@
+/**
+ * Evidence Cards
+ *
+ * lnd1_i01 - Food
+ * lnd1_i02 - Papers
+ * lnd1_i03 - Footprints and Traces
+ * lnd1_i04 - Vehicles
+ * lnd1_i05 - Blood and Organs
+ * lnd1_i06 - Liquids
+ * lnd1_i07 - Bottles
+ * lnd1_i08 - Drugs and Medicine
+ * lnd1_i09 - Gerdening Tools
+ * lnd1_i10 - Decoration Items
+ * lnd1_i11 - Jewelry
+ * lnd1_i12 - Melee Weapons
+ * lnd1_i13 - Furniture
+ * lnd1_i14 - Firearms
+ * lnd1_i15 - Communication Devices
+ * lnd1_i16 - Cords and Restraining Devices
+ * lnd1_i17 - Keys and Locks
+ * lnd1_i18 - Computer Devices
+ * lnd1_i19 - High Tech Devices
+ * lnd1_i20 - Tableware
+ * lnd1_i21 - Photos and Cameras
+ * lnd1_i22 - Kitchen Utensils
+ * lnd1_i23 - Sport Items
+ * lnd1_i24 - Books and Magazines
+ * lnd1_i25 - Office Materials
+ * lnd1_i26 - Bedding
+ * lnd1_i27 - Clothes
+ * lnd1_i28 - Animals
+ * lnd1_i29 - DIY Tools
+ * lnd1_i30 - Bags and Fashion
+ * lnd1_i31 - Games and Toys
+ * lnd1_i32 - Plants
+ * lnd1_i33 - Toiletries and Hygiene
+ * lnd1_i34 - Flammable Objects
+ * lnd1_i35 - Trash
+ * lnd1_i36 - Boxes and Packages
+ * lnd1_i37 - Signs and Symbols
+ */
