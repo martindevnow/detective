c1e8172cf693487d170cf15ac71d7fed9d329352
Benjamin Martin <ben@martindevnow.com>
2019-01-30 00:10:02 -0500

    Fix unit tests

--- tests/unit/models/person.spec.js
+++ tests/unit/models/person.spec.js
@@ -5,22 +5,22 @@ describe('Person Class', () => {
   it('returns the proper question for NO trigger', () => {
     const triggers = [];
     console.log(mockPerson.askAboutTopic('lnd1_c02', triggers));
-    expect(mockPerson.askAboutTopic('lnd1_c02', triggers))
+    expect(mockPerson.askAboutTopic('lnd1_c02', triggers).answer)
       .to.equal('DEFAULT')
   });
   it('returns the proper question for GOOD trigger', () => {
     const triggers = ['DO_SOMETHING_GOOD'];
-    expect(mockPerson.askAboutTopic('lnd1_c02', triggers))
+    expect(mockPerson.askAboutTopic('lnd1_c02', triggers).answer)
       .to.equal('VERY_HAPPY')
   });
   it('returns the proper question for BAD trigger', () => {
     const triggers = ['DO_SOMETHING_BAD'];
-    expect(mockPerson.askAboutTopic('lnd1_c02', triggers))
+    expect(mockPerson.askAboutTopic('lnd1_c02', triggers).answer)
       .to.equal('NOT_HAPPY')
   });
   it('returns the proper question for GOOD and BAD trigger', () => {
     const triggers = ['DO_SOMETHING_GOOD', 'DO_SOMETHING_BAD'];
-    expect(mockPerson.askAboutTopic('lnd1_c02', triggers))
+    expect(mockPerson.askAboutTopic('lnd1_c02', triggers).answer)
       .to.equal('MIXED_FEELING')
   });
 });
